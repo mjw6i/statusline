15d1d6c012bff922a3a5f339d5509db8b2ad8bb5
mjw6i <w6i@tuta.io>
2022-07-23 13:26:59 +0200

    move panel definitions to separate file

---
+++ panel.go
@@ -0,0 +1,22 @@
+package main
+
+type version struct {
+	Version int `json:"version"`
+}
+
+type panel struct {
+	Name                string `json:"name"`
+	Text                string `json:"full_text"`
+	Background          string `json:"background,omitempty"`
+	Color               string `json:"color,omitempty"`
+	Separator           bool   `json:"separator" default:false`
+	SeparatorBlockWidth int    `json:"separator_block_width" default:0`
+}
+
+func NewGoodPanel(name string, text string) panel {
+	return panel{Name: name, Text: text}
+}
+
+func NewBadPanel(name string, text string) panel {
+	return panel{Name: name, Text: text, Color: base, Background: accent}
+}
