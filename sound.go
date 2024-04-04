a8930ac0ddd2b3f0c1e72df6c2630c33e72f6010
mjw6i <w6i@tuta.io>
2024-04-04 16:39:57 +0200

    split

---
+++ sound.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func subscribe(updateMic, updateVolume chan<- struct{}) {
+	// use interruptable command to clean exit
+	cmd := exec.Command("pactl", "--format=json", "subscribe")
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	decoder := json.NewDecoder(out)
+
+	type event struct {
+		Event string
+		On    string
+	}
+
+	for decoder.More() {
+		var e event
+		err = decoder.Decode(&e)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch e.On {
+		case "source":
+			updateMic <- struct{}{}
+		case "sink":
+			updateVolume <- struct{}{}
+		}
+	}
+}
+
+func getMics() panel {
+	out, err := exec.Command("pactl", "--format=json", "list", "sources").Output()
+	if err != nil {
+		return NewBadPanel("mics", "error")
+	}
+
+	type source struct {
+		Properties struct {
+			DeviceClass string `json:"device.class"`
+		}
+		Mute bool
+	}
+
+	var sources []source
+	err = json.Unmarshal(out, &sources)
+	if err != nil {
+		return NewBadPanel("mics", "error")
+	}
+
+	var count int
+	var muted bool
+
+	for _, s := range sources {
+		if s.Properties.DeviceClass == "monitor" {
+			continue
+		}
+		count++
+		muted = s.Mute
+	}
+
+	if count == 0 {
+		return NewGoodPanel("mics", "")
+	}
+
+	if count > 1 {
+		return NewBadPanel("mics", " multiple mics ")
+	}
+
+	if muted {
+		return NewGoodPanel("mics", "")
+	}
+
+	return NewBadPanel("mics", " not muted ")
+}
+
+func readVolume() (int64, error) {
+	out, err := exec.Command("pactl", "get-sink-volume", "@DEFAULT_SINK@").Output()
+	if err != nil {
+		return 0, err
+	}
+
+	format := "Volume: front-left: %d / %d%% / %f dB, front-right: %d / %d%% / %f dB \n balance %f"
+
+	var fla, flp, fra, frp int64
+	var flo, fro, balance float64
+
+	_, err = fmt.Sscanf(string(out), format, &fla, &flp, &flo, &fra, &frp, &fro, &balance)
+	if err != nil {
+		return 0, err
+	}
+
+	return flp, nil
+}
+
+func volume(vol int64, err error, hide bool) panel {
+	if err != nil {
+		return NewBadPanel("volume", "error")
+	}
+
+	if hide {
+		return NewGoodPanel("volume", "")
+	}
+
+	return NewGoodPanel("volume", fmt.Sprintf(" VOL: %d%% ", vol))
+}
