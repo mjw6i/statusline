bc20f1e3ab4d7e1d6afedfba4f62260c37fbc57f
mjw6i <w6i@tuta.io>
2024-04-11 08:10:37 +0200

    fix

--- internal/date_test.go
+++ internal/date_test.go
@@ -9,7 +9,7 @@ func BenchmarkGetDate(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		d.getDate()
-		if len(d.buf) != 27 {
+		if len(d.buf) != 25 {
 			b.Fatalf("%s %v\n", d.buf, len(d.buf))
 		}
 		d.buf = d.buf[:0]
